423562616a6319494df9b1632b19523046db86a6
Skeeze1 <144681643+Skeeze1@users.noreply.github.com>
2023-10-25 10:15:56 +0300

    Add files via upload

---
+++ SQL.cpp
@@ -0,0 +1,14 @@
+// SQL.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+}
+
